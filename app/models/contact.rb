f1d8c2184c38c1454d57a9438b37d2afec7d847f
Victor Gabriel <victorgabriel27021@gmail.com>
2026-09-16 14:51:30 -0300

    autor add

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,15 @@
 class Contact < ApplicationRecord
 	belongs_to :kind
+
+	def author
+		"Victor"
+	end
+
+	def as_json(options={})
+		super(
+			root: true,
+			methods: :author,
+			include: :kind
+		)
+	end
 end
